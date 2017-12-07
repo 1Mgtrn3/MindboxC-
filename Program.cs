12eb3db6407ef17143eadc7021ec35f2e7f8f9fc
1Mgtrn3 <31386897+1Mgtrn3@users.noreply.github.com>
2017-12-07 19:04:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightTriangleLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+            do
+            {
+                triangle t = input();
+
+
+
+                TriangleChecker TC = new TriangleChecker(t);
+                if (TC.IsValid())
+                {
+
+                    Console.WriteLine("The area of your triangle is " + t.GetArea().ToString());
+                    Console.ReadKey();
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("This is not the right triangle. Try again");
+
+                }
+            } while (done == false);
+            
+
+
+
+        }
+
+        static triangle input()
+        {
+            Console.WriteLine("Enter the sides of your right triangle. Use Enter Key to split them.");
+            double FirstSide;
+            string line1 = Console.ReadLine();
+            while (!Double.TryParse(line1, out FirstSide))
+            {
+                Console.WriteLine("Not a valid number, try again.");
+
+                line1 = Console.ReadLine();
+            }
+
+            double SecondSide;
+            string line2 = Console.ReadLine();
+            while (!Double.TryParse(line2, out SecondSide))
+            {
+                Console.WriteLine("Not a valid number, try again.");
+
+                line2 = Console.ReadLine();
+            }
+
+            double ThirdSide;
+            string line3 = Console.ReadLine();
+            while (!Double.TryParse(line3, out ThirdSide))
+            {
+                Console.WriteLine("Not a valid number, try again.");
+
+                line3 = Console.ReadLine();
+            }
+            return new triangle(FirstSide, SecondSide, ThirdSide);
+        }
+    }
+}
---
+++ triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RightTriangleLib
+{
+
+    
+    public class triangle
+    {
+        private double FirstSide;
+        private double SecondSide;
+        private double ThirdSide;
+
+        public triangle(double first, double second, double third )
+        {
+            FirstSide = first;
+            SecondSide = second;
+            ThirdSide = third;
+        }
+
+        public double FirstSide1 { get => FirstSide; set => FirstSide = value; }
+        public double SecondSide1 { get => SecondSide; set => SecondSide = value; }
+        public double ThirdSide1 { get => ThirdSide; set => ThirdSide = value; }
+
+        public double GetArea(){
+            
+                var p = (FirstSide + SecondSide + ThirdSide) * 0.5; 
+                return Math.Sqrt(p * (p - FirstSide) * (p - SecondSide) * (p - ThirdSide));
+            
+        }
+
+    }
+}
